ef2c52907b4b84ed8da7accb6450766d0c21ca93
derek <44205539+derekharrison@users.noreply.github.com>
2026-03-22 19:25:18 +0100

    Refactor Brownie

--- Brownie.swift
+++ Brownie.swift
@@ -9,62 +9,49 @@ import Foundation
 import SpriteKit
 
 class Brownie : GameObject {
-    
-    override init() { super.init() }
-    
     func update(scene : SKScene) {
         super.update()
-        self.reflect(scene : scene)
-        self.updatePosition(scene : scene, at_screen : 10)
+        reflect(scene : scene)
+        updatePosition(scene : scene, at : 10)
         updateImage(numFrames: NUM_FRAMES_BIRD)
+        updateImagePositions()
+        displayImage(imageId: 0)
+        checkIfBrownieAppeared(scene: scene)
+    }
+    
+    override func displayImage(imageId _: Int) {
         if hit {
             self.displayImageNotHit(imageId: 1)
         }
-        
         if !hit {
             self.displayImageNotHit(imageId: 0)
         }
-        
-        self.appeared = self.images[0].position.x > 0 && self.images[0].position.x < scene.size.width
     }
     
-    func updatePosition(scene : SKScene, at_screen : Int) {
-        
-        self.images[0].position.x += self.velX
-        self.images[0].position.y += self.velY
-        
-        if self.images[0].position.x < -self.images[0].size.width {
-            
-            self.playSound = true
-            self.playHitSound = true
-            self.hit = false
-            
+    func checkIfBrownieAppeared(scene : SKScene) {
+        appeared = posX > 0 && posX < scene.size.width
+    }
+    
+    func updatePosition(scene : SKScene, at : Int) {
+        posX += self.velX
+        posY += self.velY
+        if posX < -self.images[0].size.width {
             let factor = 1.0 - (self.images[0].size.height) / (scene.size.height / 2)
-            self.images[0].position.x = getRandomNumber() * scene.size.width + CGFloat(at_screen) * scene.size.width
-            self.images[0].position.y = getRandomNumber() * scene.size.height / 2 * factor  + scene.size.height / 4 + 1/2 * (1 - factor) * scene.size.height / 2
+            posX = getRandomNumber() * scene.size.width + CGFloat(at) * scene.size.width
+            posY = getRandomNumber() * scene.size.height / 2 * factor  + scene.size.height / 4 + 1/2 * (1 - factor) * scene.size.height / 2
+            playSoundAllowed()
+            self.hit = false
         }
-        for x in self.images {
-            x.position = self.images[0].position
-        }
-        
-        for x in self.imagesHit {
-            x.position = self.images[0].position
-        }
-        
-        self.posX = images[0].position.x
-        self.posY = images[0].position.y
     }
-    
+
     func reflect(scene : SKScene) {
-        if self.images[0].position.y < 0.25 * scene.size.height {
-            self.images[0].position.y = 0.25 * scene.size.height
-            self.velY = -self.velY
+        if posY < 0.25 * scene.size.height {
+            posY = 0.25 * scene.size.height
+            velY = -velY
         }
-        if self.images[0].position.y > 0.75 * scene.size.height {
-            self.images[0].position.y = 0.75 * scene.size.height
-            self.velY = -self.velY
+        if posY > 0.75 * scene.size.height {
+            posY = 0.75 * scene.size.height
+            velY = -velY
         }
-        self.posX = images[0].position.x
-        self.posY = images[0].position.y
     }
 }

8ea96d7d461b874f4b3f79bec7a7613504dcde63
derek <44205539+derekharrison@users.noreply.github.com>
2026-03-22 16:53:12 +0100

    Refactor bubbles

--- Bubbles.swift
+++ Bubbles.swift
@@ -10,30 +10,30 @@ import SpriteKit
 
 class Bubbles {
     
-    var bubbles : [SKSpriteNode]
+    var bubblez : [SKSpriteNode]
     var bubcounter : Int
     var isMuted : Bool
     
     init() {
-        self.bubbles = [SKSpriteNode]()
+        self.bubblez = [SKSpriteNode]()
         self.bubcounter = 1
         self.isMuted = false
     }
     
     func addBubble(imageId : String) {
         
-        self.bubbles.append(SKSpriteNode(imageNamed: imageId))
+        self.bubblez.append(SKSpriteNode(imageNamed: imageId))
     }
     
     func playBubbles() {
         bubcounter = 1
-        for x in bubbles {
+        for x in bubblez {
             x.zPosition = -1
         }
     }
     
     func setPosition(pos : CGPoint) {
-        for x in bubbles {
+        for x in bubblez {
             x.position = pos
         }
     }
@@ -41,8 +41,8 @@ class Bubbles {
     func popBubbles(pos : CGPoint, scene : SKScene, sound : [SKAction]) {
         
         if self.bubcounter == 1 {
-            self.bubbles[0].position = pos
-            self.bubbles[0].zPosition = 1000
+            self.bubblez[0].position = pos
+            self.bubblez[0].zPosition = 1000
             self.bubcounter += 1
             if self.isMuted == false {
                 play_sound_api(scene : scene, sound : sound)
@@ -50,28 +50,28 @@ class Bubbles {
             
         }
         else if self.bubcounter < 40 {
-            self.bubbles[0].position.y += bubble_vel_y
+            self.bubblez[0].position.y += bubble_vel_y
             self.bubcounter += 1
         }
         else if bubcounter == 40 {
-            self.bubbles[1].position = pos
-            self.bubbles[1].zPosition = 1000
+            self.bubblez[1].position = pos
+            self.bubblez[1].zPosition = 1000
             self.bubcounter += 1
         }
         else if bubcounter < 80 {
-            self.bubbles[0].position.y += bubble_vel_y
-            self.bubbles[1].position.y += bubble_vel_y
+            self.bubblez[0].position.y += bubble_vel_y
+            self.bubblez[1].position.y += bubble_vel_y
             self.bubcounter += 1
         }
         else if self.bubcounter == 80 {
-            self.bubbles[2].position = pos
-            self.bubbles[2].zPosition = 1000
+            self.bubblez[2].position = pos
+            self.bubblez[2].zPosition = 1000
             self.bubcounter += 1
         }
         else if self.bubcounter < 240 {
-            self.bubbles[0].position.y += bubble_vel_y
-            self.bubbles[1].position.y += bubble_vel_y
-            self.bubbles[2].position.y += bubble_vel_y
+            self.bubblez[0].position.y += bubble_vel_y
+            self.bubblez[1].position.y += bubble_vel_y
+            self.bubblez[2].position.y += bubble_vel_y
             self.bubcounter += 1
         }
         if self.bubcounter == 240 {

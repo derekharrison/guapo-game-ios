748e17de767ea0404cc12a569f3c3d6b102e8e4b
derek <44205539+derekharrison@users.noreply.github.com>
2026-03-23 19:22:42 +0100

    Refactor

--- PlayerScene.swift
+++ PlayerScene.swift
@@ -10,26 +10,26 @@ import SpriteKit
 
 class PlayerScene: SKScene {
     
-    let level_menu_np = SKSpriteNode(imageNamed: CONTINUE_NOTPRESSED)
-    let level_menu_p = SKSpriteNode(imageNamed: CONTINUE_PRESSED)
-    let start_menu_np = SKSpriteNode(imageNamed: START_MENU_NOTPRESSED)
-    let start_menu_p = SKSpriteNode(imageNamed: START_MENU_PRESSED)
+    let continueNotPressed = SKSpriteNode(imageNamed: CONTINUE_NOTPRESSED)
+    let continuePressed = SKSpriteNode(imageNamed: CONTINUE_PRESSED)
+    let startMenuNotPressed = SKSpriteNode(imageNamed: START_MENU_NOTPRESSED)
+    let startMenuPressed = SKSpriteNode(imageNamed: START_MENU_PRESSED)
     
-    let guapo_image_i = SKSpriteNode(imageNamed: GUAPO_IMAGE_PLAYER)
-    let tutti_image_i = SKSpriteNode(imageNamed: TUTTI_IMAGE_PLAYER)
+    let guapoImage = SKSpriteNode(imageNamed: GUAPO_IMAGE_PLAYER)
+    let tuttiImage = SKSpriteNode(imageNamed: TUTTI_IMAGE_PLAYER)
 
-    let guapo_button_np = SKSpriteNode(imageNamed: GUAPO_BUTTON_NOTPRESSED)
-    let guapo_button_p = SKSpriteNode(imageNamed: GUAPO_BUTTON_PRESSED)
-    let tutti_button_np = SKSpriteNode(imageNamed: TUTTI_BUTTON_NOTPRESSED)
-    let tutti_button_p = SKSpriteNode(imageNamed: TUTTI_BUTTON_PRESSED)
+    let guapoButtonNotPressed = SKSpriteNode(imageNamed: GUAPO_BUTTON_NOTPRESSED)
+    let guapoButtonPressed = SKSpriteNode(imageNamed: GUAPO_BUTTON_PRESSED)
+    let tuttiButtonNotPressed = SKSpriteNode(imageNamed: TUTTI_BUTTON_NOTPRESSED)
+    let tuttiButtonPressed = SKSpriteNode(imageNamed: TUTTI_BUTTON_PRESSED)
     
-    var start_menu_touched = false
-    var start_menu = false
-    var start_menu_scene = false
+    var startMenuChosen = false
+    var startMenu = false
+    var startMenuScene = false
     
-    var level_menu_touched = false
-    var level_menu = false
-    var level_menu_scene = false
+    var levelMenuChosen = false
+    var levelMenu = false
+    var levelMenuScene = false
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: BACKGROUND_START_SCREEN)
@@ -44,42 +44,38 @@ class PlayerScene: SKScene {
         background.removeFromParent()
         self.addChild(background)
         
-        place_image(fac: 0.8, button: guapo_image_i)
-        place_image(fac: 0.66, button: tutti_image_i)
+        placeImage(fac: 0.8, button: guapoImage)
+        placeImage(fac: 0.66, button: tuttiImage)
         
-        place_button(fac: 0.8, button_np: guapo_button_np, button_p: guapo_button_p)
-        place_button(fac: 0.66, button_np: tutti_button_np, button_p: tutti_button_p)
-        
-        place_button2(fac: 0.3, button_np: level_menu_np, button_p: level_menu_p)
-        place_button2(fac: 0.16, button_np: start_menu_np, button_p: start_menu_p)
+        placeButton(fac: 0.8, buttonNotPressed: guapoButtonNotPressed, buttonPressed: guapoButtonPressed, width: self.size.width / 5, height: self.size.height / 16)
+        placeButton(fac: 0.66, buttonNotPressed: tuttiButtonNotPressed, buttonPressed: tuttiButtonPressed, width: self.size.width / 5, height: self.size.height / 16)
+        placeButton(fac: 0.3, buttonNotPressed: continueNotPressed, buttonPressed: continuePressed, width: self.size.width / 4, height: self.size.height / 16)
+        placeButton(fac: 0.16, buttonNotPressed: startMenuNotPressed, buttonPressed: startMenuPressed, width: self.size.width / 4, height: self.size.height / 16)
     }
     
     //Force the mute button to change by running update()
     override func update(_ currentTime: TimeInterval) {
-        startScene(scene: self, start : &start_menu, GameLevel : StartMenu(size: self.size))
-        startScene(scene: self, start : &level_menu, GameLevel : MainMenuScene(size: self.size))
+        startScene(scene: self, start : &startMenu, GameLevel : StartMenu(size: self.size))
+        startScene(scene: self, start : &levelMenu, GameLevel : MainMenuScene(size: self.size))
         
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+    override func touchesBegan(_ touches: Set<UITouch>, with _: UIEvent?) {
         for touch: AnyObject in touches {
-            
             let pointOfTouch = touch.location(in: self)
-    
+            
             var dummy1 = false
             var dummy2 = false
             
-            touched_button(pointOfTouch: pointOfTouch, level_np : start_menu_np, level_p : start_menu_p, touched : &start_menu)
-            touched_button(pointOfTouch: pointOfTouch, level_np : level_menu_np, level_p : level_menu_p, touched : &level_menu)
-            touched_button2(pointOfTouch: pointOfTouch, level_np : guapo_button_np, level_p : guapo_button_p, touched : &dummy1)
-            touched_button2(pointOfTouch: pointOfTouch, level_np : tutti_button_np, level_p : tutti_button_p, touched : &dummy2)
+            pressedMenuButton(pointOfTouch: pointOfTouch, level_np : startMenuNotPressed, level_p : startMenuPressed, touched : &startMenu)
+            pressedMenuButton(pointOfTouch: pointOfTouch, level_np : continueNotPressed, level_p : continuePressed, touched : &levelMenu)
+            pressedCharacterMenu(pointOfTouch: pointOfTouch, level_np : guapoButtonNotPressed, level_p : guapoButtonPressed, touched : &dummy1)
+            pressedCharacterMenu(pointOfTouch: pointOfTouch, level_np : tuttiButtonNotPressed, level_p : tuttiButtonPressed, touched : &dummy2)
             
             if dummy1 {
                 let defaults = UserDefaults()
                 defaults.set(0, forKey: "player_id")
             }
-            
             if dummy2 {
                 let defaults = UserDefaults()
                 defaults.set(1, forKey: "player_id")
@@ -87,33 +83,33 @@ class PlayerScene: SKScene {
         }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if start_menu_touched {
-            start_menu = true
-            start_menu_scene = true
+    override func touchesEnded(_ _: Set<UITouch>, with _: UIEvent?) {
+        if startMenuChosen {
+            startMenu = true
+            startMenuScene = true
         }
-        if level_menu_touched {
-            level_menu = true
-            level_menu_scene = true
+        if levelMenuChosen {
+            levelMenu = true
+            levelMenuScene = true
         }
     }
     
-    func place_button(fac : CGFloat, button_np : SKSpriteNode, button_p : SKSpriteNode) {
+    func placeButton(fac : CGFloat, buttonNotPressed : SKSpriteNode, buttonPressed : SKSpriteNode, width : CGFloat, height : CGFloat) {
         
-        let width_button = self.size.width / 5
-        let height_button = self.size.height / 16
+        let width_button = width
+        let height_button = height
         
-        button_np.size = CGSize(width: width_button, height: height_button)
-        button_np.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2 * fac + self.size.height / 4)
-        button_np.zPosition = 2
-        button_np.removeFromParent()
-        self.addChild(button_np)
+        buttonNotPressed.size = CGSize(width: width_button, height: height_button)
+        buttonNotPressed.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2 * fac + self.size.height / 4)
+        buttonNotPressed.zPosition = 2
+        buttonNotPressed.removeFromParent()
+        self.addChild(buttonNotPressed)
         
-        button_p.size = CGSize(width: width_button, height: height_button)
-        button_p.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2 * fac + self.size.height / 4)
-        button_p.zPosition = -1
-        button_p.removeFromParent()
-        self.addChild(button_p)
+        buttonPressed.size = CGSize(width: width_button, height: height_button)
+        buttonPressed.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2 * fac + self.size.height / 4)
+        buttonPressed.zPosition = -1
+        buttonPressed.removeFromParent()
+        self.addChild(buttonPressed)
     }
     
     func place_button2(fac : CGFloat, button_np : SKSpriteNode, button_p : SKSpriteNode) {
@@ -134,19 +130,19 @@ class PlayerScene: SKScene {
         self.addChild(button_p)
     }
     
-    func place_image(fac : CGFloat, button : SKSpriteNode) {
+    func placeImage(fac : CGFloat, button : SKSpriteNode) {
         
         let width_button = self.size.width / 16
         let height_button = self.size.height / 16
         
         button.size = CGSize(width: width_button, height: height_button)
-        button.position = CGPoint(x: self.size.width / 2 - self.guapo_image_i.size.width * 3, y: self.size.height / 2 * fac + self.size.height / 4)
+        button.position = CGPoint(x: self.size.width / 2 - self.guapoImage.size.width * 3, y: self.size.height / 2 * fac + self.size.height / 4)
         button.zPosition = 2
         button.removeFromParent()
         self.addChild(button)
     }
     
-    func touched_button(pointOfTouch : CGPoint, level_np : SKSpriteNode, level_p : SKSpriteNode, touched : inout Bool) {
+    func pressedMenuButton(pointOfTouch : CGPoint, level_np : SKSpriteNode, level_p : SKSpriteNode, touched : inout Bool) {
     
         
         let touched_button_x = pointOfTouch.x > level_np.position.x - level_np.size.width / 2 && pointOfTouch.x < level_np.position.x + level_np.size.width / 2
@@ -161,7 +157,7 @@ class PlayerScene: SKScene {
         }
     }
     
-    func touched_button2(pointOfTouch : CGPoint, level_np : SKSpriteNode, level_p : SKSpriteNode, touched : inout Bool) {
+    func pressedCharacterMenu(pointOfTouch : CGPoint, level_np : SKSpriteNode, level_p : SKSpriteNode, touched : inout Bool) {
     
         
         let touched_button_x = pointOfTouch.x > level_np.position.x - level_np.size.width / 2 && pointOfTouch.x < level_np.position.x + level_np.size.width / 2
@@ -169,10 +165,10 @@ class PlayerScene: SKScene {
         let touched_button_y = pointOfTouch.y > level_np.position.y - level_np.size.height / 2 && pointOfTouch.y < level_np.position.y + level_np.size.height / 2
         
         if touched_button_x && touched_button_y {
-            self.guapo_button_np.zPosition = 2
-            self.guapo_button_p.zPosition = -1
-            self.tutti_button_np.zPosition = 2
-            self.tutti_button_p.zPosition = -1
+            self.guapoButtonNotPressed.zPosition = 2
+            self.guapoButtonPressed.zPosition = -1
+            self.tuttiButtonNotPressed.zPosition = 2
+            self.tuttiButtonPressed.zPosition = -1
             
             level_np.zPosition = -1
             level_p.zPosition = 2

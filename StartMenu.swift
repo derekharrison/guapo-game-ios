85d3e86860ecf65a8112a3a07b8213c389296c5a
derek <44205539+derekharrison@users.noreply.github.com>
2026-03-23 19:03:44 +0100

    Refactor start menu

--- StartMenu.swift
+++ StartMenu.swift
@@ -11,18 +11,18 @@ import SpriteKit
 
 class StartMenu: SKScene {
     
-    let player_menu_np = SKSpriteNode(imageNamed: PLAYER_MENU_BUTTON_NOTPRESSED)
-    let player_menu_p = SKSpriteNode(imageNamed: PLAYER_MENU_BUTTON_PRESSED)
-    let level_menu_np = SKSpriteNode(imageNamed: LEVEL_MENU_NOTPRESSED)
-    let level_menu_p = SKSpriteNode(imageNamed: LEVEL_MENU_PRESSED)
+    let playerMenuNotPressed = SKSpriteNode(imageNamed: PLAYER_MENU_BUTTON_NOTPRESSED)
+    let playerMenuPressed = SKSpriteNode(imageNamed: PLAYER_MENU_BUTTON_PRESSED)
+    let levelMenuNotPressed = SKSpriteNode(imageNamed: LEVEL_MENU_NOTPRESSED)
+    let levelMenuPressed = SKSpriteNode(imageNamed: LEVEL_MENU_PRESSED)
     
-    var player_menu_touched = false
-    var player_menu = false
-    var player_menu_scene = false
+    var playerMenuChosen = false
+    var playerMenu = false
+    var playerMenuScene = false
     
-    var level_menu_touched = false
-    var level_menu = false
-    var level_menu_scene = false
+    var levelMenuChosen = false
+    var levelMenu = false
+    var levelMenuScene = false
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: BACKGROUND_START_SCREEN)
@@ -37,40 +37,37 @@ class StartMenu: SKScene {
         background.removeFromParent()
         self.addChild(background)
         
-        place_button(fac: 0.6, button_np: player_menu_np, button_p: player_menu_p)
-        place_button(fac: 0.46, button_np: level_menu_np, button_p: level_menu_p)
+        placeButton(fac: 0.6, button_np: playerMenuNotPressed, button_p: playerMenuPressed)
+        placeButton(fac: 0.46, button_np: levelMenuNotPressed, button_p: levelMenuPressed)
     }
     
     //Force the mute button to change by running update()
     override func update(_ currentTime: TimeInterval) {
    
-        startScene(scene: self, start : &player_menu, GameLevel : PlayerScene(size: self.size))
-        startScene(scene: self, start : &level_menu, GameLevel : MainMenuScene(size: self.size))
+        startScene(scene: self, start : &playerMenu, GameLevel : PlayerScene(size: self.size))
+        startScene(scene: self, start : &levelMenu, GameLevel : MainMenuScene(size: self.size))
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
         for touch: AnyObject in touches {
-            
             let pointOfTouch = touch.location(in: self)
-        
-            touched_button(pointOfTouch: pointOfTouch, level_np : player_menu_np, level_p : player_menu_p, start_level_touched : &player_menu)
-            touched_button(pointOfTouch: pointOfTouch, level_np : level_menu_np, level_p : level_menu_p, start_level_touched : &level_menu)
+            pressedButton(pointOfTouch: pointOfTouch, levelNotPressed : playerMenuNotPressed, levelPressed : playerMenuPressed, pressed : &playerMenu)
+            pressedButton(pointOfTouch: pointOfTouch, levelNotPressed : levelMenuNotPressed, levelPressed : levelMenuPressed, pressed : &levelMenu)
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if player_menu_touched {
-            player_menu = true
-            player_menu_scene = true
+        if playerMenuChosen {
+            playerMenu = true
+            playerMenuScene = true
         }
-        if level_menu_touched {
-            level_menu = true
-            level_menu_scene = true
+        if levelMenuChosen {
+            levelMenu = true
+            levelMenuScene = true
         }
     }
     
-    func place_button(fac : CGFloat, button_np : SKSpriteNode, button_p : SKSpriteNode) {
+    private func placeButton(fac : CGFloat, button_np : SKSpriteNode, button_p : SKSpriteNode) {
         
         let width_button = self.size.width / 4
         let height_button = self.size.height / 16
@@ -88,15 +85,15 @@ class StartMenu: SKScene {
         self.addChild(button_p)
     }
     
-    func touched_button(pointOfTouch : CGPoint, level_np : SKSpriteNode, level_p : SKSpriteNode, start_level_touched : inout Bool) {
-        let touched_button_x = pointOfTouch.x > level_np.position.x - level_np.size.width / 2 && pointOfTouch.x < level_np.position.x + level_np.size.width / 2
+    private func pressedButton(pointOfTouch : CGPoint, levelNotPressed : SKSpriteNode, levelPressed : SKSpriteNode, pressed : inout Bool) {
+        let inRangeX = pointOfTouch.x > levelNotPressed.position.x - levelNotPressed.size.width / 2 && pointOfTouch.x < levelNotPressed.position.x + levelNotPressed.size.width / 2
         
-        let touched_button_y = pointOfTouch.y > level_np.position.y - level_np.size.height / 2 && pointOfTouch.y < level_np.position.y + level_np.size.height / 2
+        let inRangeY = pointOfTouch.y > levelNotPressed.position.y - levelNotPressed.size.height / 2 && pointOfTouch.y < levelNotPressed.position.y + levelNotPressed.size.height / 2
         
-        if touched_button_x && touched_button_y {
-            level_np.zPosition = -1
-            level_p.zPosition = 2
-            start_level_touched = true
+        if inRangeX && inRangeY {
+            levelNotPressed.zPosition = -1
+            levelPressed.zPosition = 2
+            pressed = true
         }
     }
 }
